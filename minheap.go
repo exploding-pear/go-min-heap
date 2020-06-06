76567c9eaf28eb64f63881c8b0525fa9db903c4f
exploding-pear <alexjdupper@gmail.com>
2020-06-06 15:27:24 -0400

    updated minheap to use array/slice

--- minheap.go
+++ minheap.go
@@ -3,46 +3,83 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/exploding-pear/go-min-heap/node"
+	"math/rand"
+	"time"
 )
 
-// PrintTree prints out tree
-func PrintTree(n *node.Node) {
-	ch := make(chan int)
-	list := make([]int, 0)
-	go func() {
-		defer close(ch)
-		traverseTree(n, ch)
-	}()
-	for i := range ch {
-		list = append(list, i)
+// PrintTree prints out a binary tree
+func PrintTree(t []int) {
+	for i := range t {
+		fmt.Println(t[i])
 	}
-	fmt.Println(list)
 }
 
-func traverseTree(n *node.Node, c chan int) {
-	if n == nil {
-		return
+// RandomTree builds a random binary tree of Nodes
+// with a maximum size bounded by maxsize and each
+// node having a value of at most maxvalue
+func RandomTree(maxsize int, maxvalue int) []int {
+	rand.Seed(time.Now().UnixNano())
+	randomsize := rand.Int() % (maxsize + 1)
+	tree := make([]int, randomsize)
+
+	// Build a list of orphan nodes of random values
+	for i := 0; i < len(tree); i++ {
+		tree[i] = rand.Int() % (maxvalue + 1)
 	}
-	c <- n.GetValue()
-	traverseTree(n.LeftChild, c)
-	traverseTree(n.RightChild, c)
+	return tree
+}
+
+func heapSort(arr []int) {
+	var temp int
+	n := len(arr)
+	// Build heap (rearrange array)
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(arr, n, i)
+	}
+	// One by one extract an element from heap
+	for i := n - 1; i > 0; i-- {
+		// Move current root to end
+		temp = arr[0]
+		arr[0] = arr[i]
+		arr[i] = temp
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0)
+	}
+}
+
+// SortTree sorts the binary tree into a min heap
+func heapify(arr []int, n int, i int) {
+	largest := i // Initialize largest as root
+	l := 2*i + 1 // left = 2*i + 1
+	r := 2*i + 2 // right = 2*i + 2
+	var temp int
+
+	// If left child is larger than root
+	if l < n && arr[l] > arr[largest] {
+		largest = l
+	}
+	// If right child is larger than largest so far
+	if r < n && arr[r] > arr[largest] {
+		largest = r
+	}
+	// If largest is not root
+	if largest != i {
+		// swap values
+		temp = arr[i]
+		arr[i] = arr[largest]
+		arr[largest] = temp
+
+		// Recursively heapify the affected sub-tree
+		heapify(arr, n, largest)
+	}
 }
 
 func main() {
-	n1 := node.NewOrphan(4)
-	n2 := node.NewOrphan(10)
-	n3 := node.NewOrphan(15)
-	n4 := node.NewOrphan(43)
-	n5 := node.NewOrphan(71)
-	n6 := node.NewOrphan(400)
-
-	n1.NewChild(&n2)
-	n1.NewChild(&n3)
-	n2.NewChild(&n4)
-	n2.NewChild(&n5)
-	n3.NewChild(&n6)
-
-	PrintTree(&n1)
+	tree := RandomTree(15, 50)
+	fmt.Println("tree before sort")
+	PrintTree(tree)
+	fmt.Println("tree after sort")
+	heapSort(tree)
+	PrintTree(tree)
 }
